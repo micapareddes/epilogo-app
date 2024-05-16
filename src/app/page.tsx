2d42af24dfe54b79ce352100d86bf09eed1c0dec
micapareddes <micapareddes@gmail.com>
2024-05-16 11:35:52 -0300

    style: Created HeroSection component to encapsulate title, subtitle, and CTA for the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import { Button } from '@/components/button/button'
-import { H1 } from '@/components/titles/h1'
+import { HeroSection } from '@/components/hero-section'
 
 export default function Home() {
-  return (
-    <main className="h-screen flex flex-col items-center justify-center px-m">
-      <H1 className="flex text-center mb-xs">
-        Mantenha suas
-        <br className="hidden md:block" /> leituras organizadas!
-      </H1>
-      <p className="max-w-lg text-p4 md:text-p2 text-center text-gray-600 dark:text-gray-200 mb-l ">
-        Crie listas, marque os dias em que leu, explore novos livros e acompanhe
-        o que seus amigos estão lendo.
-      </p>
-      <div className="flex gap-m text-p4">
-        <Button>Criar conta</Button>
-        <Button variant={'outline'}>Acessar</Button>
-      </div>
-    </main>
-  )
+  return <HeroSection />
 }
---
+++ src/components/hero-section.tsx
@@ -0,0 +1,21 @@
+import { Button } from './button/button'
+import { H1 } from './titles/h1'
+
+export function HeroSection() {
+  return (
+    <section className="h-screen flex flex-col items-center justify-center px-m">
+      <H1 className="flex text-center mb-xs">
+        Mantenha suas
+        <br className="hidden md:block" /> leituras organizadas!
+      </H1>
+      <p className="max-w-lg text-p4 md:text-p2 text-center text-gray-600 dark:text-gray-200 mb-l ">
+        Crie listas, marque os dias em que leu, explore novos livros e acompanhe
+        o que seus amigos estão lendo.
+      </p>
+      <div className="flex gap-m text-p4">
+        <Button>Criar conta</Button>
+        <Button variant={'outline'}>Acessar</Button>
+      </div>
+    </section>
+  )
+}
